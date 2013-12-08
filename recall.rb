9130e29097b6e26521058e0510f24129f8e7c823
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2013-12-08 18:35:11 -0200

    Checked all application about catching security errors and using flash messages

--- recall.rb
+++ recall.rb
@@ -2,6 +2,12 @@ require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
 require 'sinatra/json'
+require 'sinatra/base'
+require 'rack-flash'
+require 'sinatra/redirect_with_flash'
+
+enable :sessions
+use Rack::Flash, :sweep => true
 
 SITE_TITLE = "Recall"
 SITE_DESCRIPTION = "'cause you're too busy to remember"
@@ -27,6 +33,9 @@ end
 get '/' do
   @notes = Note.all :order => :id.desc
   @title = 'All Notes'
+  if @notes.empty?
+    flash[:error] = 'No notes found. Add your first below.'
+  end
   erb :home
 end
 
@@ -35,9 +44,11 @@ post '/' do
   n.content = params[:content]
   n.created_at = Time.now
   n.updated_at = Time.now
-  n.save
-
-  redirect '/'
+  if n.save
+    redirect '/', :notice => 'Note Created succesfully.'
+  else
+    redirect '/', :error => 'Failed to save note.'
+  end
 end
 
 get '/rss.xml' do
@@ -48,38 +59,61 @@ end
 get '/:id' do
 
   @note = Note.get params[:id]
-  @title = "Editing note: #{@note.content}"
-  erb :edit
+
+  if @note
+    @title = "Editing note: #{@note.content}"
+    erb :edit
+  else
+    redirect '/', :error => "Can't find that note"
+  end
 end
 
 
 put '/:id' do
   n = Note.get params[:id]
+  unless n
+    redirect '/', :error => "Can't find that note"
+  end
   n.content = params[:content]
   n.complete = params[:complete] ? 1 : 0
   n.updated_at = Time.now
-  n.save
-
-  redirect '/'
+  if n.save
+    redirect '/', :notice => 'Note updated successfully'
+  else
+    redirect '/', :error => 'Error updating successfully'
+  end
 end
 
 get '/:id/delete' do
   @note = Note.get params[:id]
-  @title = "Confirm deletion of note ##{params[:id]}"
-  erb :delete
+  if @note
+    @title = "Confirm deletion of note ##{params[:id]}"
+    erb :delete
+  else
+    redirect '/', :error => "Can't find that note"
+  end
 end
 
 delete '/:id' do
   n = Note.get params[:id]
-  n.destroy
-  redirect '/'
+  if n.destroy
+    redirect '/', :notice => 'Note deleted successfully'
+  else
+    redirect '/', :error => 'Error while deleting note'
+  end
 end
 
 get '/:id/complete' do
   n = Note.get params[:id]
+  unless n
+    redirect '/', :error => "Can't find that note"
+  end
   n.complete = 1
-  n.save
-  redirect '/'
+  if n.save
+    redirect '/', :notice => 'Note marked as complete.'
+  else
+    redirect '/', :error => 'Eror marking note as complete'
+  end
 end
 
 get '/notes/json' do
